b83b332980b1d0996164d5b0caf6eb6365abc472
Hari Pogula <hariar2.0@outlook.com>
2018-01-22 10:59:43 -0500

    Implementing Login and Logout with Google

--- OShop/src/app/login/login.component.ts
+++ OShop/src/app/login/login.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth} from 'angularfire2/auth';
+import * as firebase from 'firebase';//Due to accessing limited features for authentication, we need to import everything.
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor(private afAuth:AngularFireAuth) {
+    
+   }
+login()
+{
+this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+}
+  
 }
