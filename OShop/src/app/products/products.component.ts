1b37e6eb3c57bf9e549bf49b13826144cebdd557
Hari Pogula <hariar2.0@outlook.com>
2018-02-13 21:54:45 -0500

    Applying Filters with Categories

--- OShop/src/app/products/products.component.ts
+++ OShop/src/app/products/products.component.ts
@@ -1,15 +1,40 @@
+import { CategoryService } from './../services/category.service';
 import { ProductService } from './../services/product.service';
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { Products } from '../classes/products';
+import 'rxjs/add/operator/switchMap';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-products$;
-  constructor(private productservice:ProductService) {
-    this.products$=this.productservice.getAll();
+  category: string;
+products:Products[]=[];
+filteredProducts:Products[];
+categories$;
+  constructor(private productservice:ProductService,
+    private categoryservice:CategoryService,
+  private route:ActivatedRoute) {
+   this.productservice
+   .getAll()
+   .switchMap(products=>
+    {
+      this.products=products;
+return this.route.queryParamMap; //Store in Params Variable in next line
+    })
+    .subscribe(params=>
+    {
+      this.category=params.get('category') //Set in hyperlink
+      //Filter
+      this.filteredProducts=(this.category)?
+      this.products.filter(p=>p.category===this.category):
+      this.products
+    })
+      
+    this.categories$=this.categoryservice.getCategories();
+   
    }
 
   ngOnInit() {
